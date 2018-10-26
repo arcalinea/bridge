34e14e8d19b1f63716dd7f502ea8875cc700c7fa
Jay Graber <jay@z.cash>
2018-10-26 14:36:38 -0700

    Mv tests to test.js

--- index.js
+++ index.js
@@ -17,26 +17,3 @@ const facebookAdaptor = require('./extensions/facebook');
 // Data
 // Response_To
 // Signature
-
-// Get raw tweets
-var tweets = twitterAdaptor.readFromDataDir(config.twitter.data_dir);
-console.log("Tweets", tweets);
-// convert raw tweets to smors
-var smors = twitterAdaptor.toSmors(tweets);
-console.log("Smors", smors);
-// // convert smor to ssb post
-var ssb_posts = ssbAdaptor.smorsToSsb(smors);
-console.log("Ssb posts", ssb_posts);
-
-var ssb_posts = ssbAdaptor.smorsToSsb(twitterAdaptor.toSmors(twitterAdaptor.readFromDataDir(config.twitter.data_dir)));
-console.log("Ssb posts", ssb_posts);
-
-var fbkPosts = facebookAdaptor.readFromDataDir(config.facebook.data_dir);
-console.log("fbkPosts", fbkPosts)
-var fbkSmors = facebookAdaptor.toSmors(fbkPosts);
-console.log("fbkSmors", fbkSmors)
-
-var allSmors = smors.concat(fbkSmors);
-console.log("Posts", allSmors)
-utils.createDataFile(allSmors);
-
---
+++ test.js
@@ -0,0 +1,42 @@
+const config = require('./config');
+const utils = require('./utils');
+
+const twitterAdaptor = require('./extensions/twitter');
+const ssbAdaptor = require('./extensions/ssb');
+const facebookAdaptor = require('./extensions/facebook');
+
+// Pull in social media data, feed into common data format. 
+//  1. From user-requested data dump
+// 	2. TODO: Through OAuth and API
+
+// Smor = social media object representation
+// Type
+// Source
+// Author
+// Created_At
+// Data
+// Response_To
+// Signature
+
+// Get raw tweets
+var tweets = twitterAdaptor.readFromDataDir(config.twitter.data_dir);
+console.log("Tweets", tweets);
+// convert raw tweets to smors
+var smors = twitterAdaptor.toSmors(tweets);
+console.log("Smors", smors);
+// // convert smor to ssb post
+var ssb_posts = ssbAdaptor.smorsToSsb(smors);
+console.log("Ssb posts", ssb_posts);
+
+var ssb_posts = ssbAdaptor.smorsToSsb(twitterAdaptor.toSmors(twitterAdaptor.readFromDataDir(config.twitter.data_dir)));
+console.log("Ssb posts", ssb_posts);
+
+var fbkPosts = facebookAdaptor.readFromDataDir(config.facebook.data_dir);
+console.log("fbkPosts", fbkPosts)
+var fbkSmors = facebookAdaptor.toSmors(fbkPosts);
+console.log("fbkSmors", fbkSmors)
+
+var allSmors = smors.concat(fbkSmors);
+console.log("Posts", allSmors)
+utils.createDataFile(allSmors);
+
